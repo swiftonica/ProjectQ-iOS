dfcda850f05d314b65b6e494976f194574529725
Jeytery <dimaostapchenko@gmail.com>
2023-03-12 14:31:10 +0200

    remove debug staff

--- ProjectQ-iOS/Application/Coordinators/TaskInformationCoordinator.swift
+++ ProjectQ-iOS/Application/Coordinators/TaskInformationCoordinator.swift
@@ -86,73 +86,23 @@ class TaskInformationCoordinator: Coordinatable {
 
 private extension TaskInformationCoordinator {
     func addComponentCoordinator() {
-        
-        let calendar = Calendar.current
-        let now = Date()
-        let fiveHourAgo = calendar.date(byAdding: .hour, value: -5, to: now)!
-
-        
-        let input1 = IntervalComponentHandlerInput(
-            intervalType: .byWeek([.fri, .mon]),
-            time: Date(),
-            lastDate: Date()
+        let componentCoordinator = ComponentsCoordinator(
+            navigationController: self.navigationController,
+            isAnimated: true
         )
-        
-        let input2 = IntervalComponentHandlerInput(
-            intervalType: .interval(5),
-            time: fiveHourAgo,
-            lastDate: Date()
-        )
-        
-        let input3 = IntervalComponentHandlerInput(
-            intervalType: .interval(20),
-            time: fiveHourAgo,
-            lastDate: fiveHourAgo
-        )
-        
-        let component1 = Component
-            .interval
-            .inputed(
-                try? JSONEncoder().encode(input1)
-            )
-        
-        let component2 = Component
-            .interval
-            .inputed(
-                try? JSONEncoder().encode(input2)
-            )
-        
-        let component3 = Component
-            .interval
-            .inputed(
-                try? JSONEncoder().encode(input3)
-            )
-        
-        let array = [component1, component2, component3]
-        self.taskInformationModule.publicInterface?.setComponents(array)
-        
-        
-        //        self.taskInformationModule.publicInterface?.addComponent(component1)
-        //        self.taskInformationModule.publicInterface?.addComponent(component2)
-        //        self.taskInformationModule.publicInterface?.addComponent(component3)
-        
-//        let componentCoordinator = ComponentsCoordinator(
-//            navigationController: self.navigationController,
-//            isAnimated: true
-//        )
-//        componentCoordinator.completion = {
-//            event in
-//            switch event {
-//            case .didChooseComponent(let component):
-//                self.remove(coordinatable: componentCoordinator)
-//                self.navigationController.popToViewController(
-//                    self.taskInformationModule.view,
-//                    animated: true
-//                )
-//                self.taskInformationModule.publicInterface?.addComponent(component)
-//            }
-//        }
-//        self.add(coordinatable: componentCoordinator)
-//        componentCoordinator.start()
+        componentCoordinator.completion = {
+            event in
+            switch event {
+            case .didChooseComponent(let component):
+                self.remove(coordinatable: componentCoordinator)
+                self.navigationController.popToViewController(
+                    self.taskInformationModule.view,
+                    animated: true
+                )
+                self.taskInformationModule.publicInterface?.addComponent(component)
+            }
+        }
+        self.add(coordinatable: componentCoordinator)
+        componentCoordinator.start()
     }
 }
